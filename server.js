2c6db3cf52ac5346c7a4b484eb761a8e3c317579
Artyom Fedenko <artyomfedenko@hotmail.com>
2015-07-16 03:40:51 +0300

    Added demo router for demo pages

--- server.js
+++ server.js
@@ -2,18 +2,24 @@
 var express   = require('express'),
     routers   = require('./lib/routers/routers');
 
-var apiRouterFunc = routers.apiRouter;
+var apiRouterFunc  = routers.apiRouter,
+    demoRouterFunc = routers.demoRouter;
 
 var app        = express(),
-    apiRouter  = express.Router();
+    apiRouter  = express.Router(),
+    demoRouter = express.Router();
 
 
-app.use("/public", express.static(`${__dirname}/public`));
+// app.use("/demo", express.static(`${__dirname}/public/demo`));
 
 // Routing
+// Api Routers
 // Params - url:String
 apiRouter.get('/convert', apiRouterFunc.get.convert);
 
+// Demo Routers
+demoRouter.get('/index', demoRouterFunc.get.root);
 
 app.use('/api', apiRouter);
+app.use('/demo', demoRouter);
 app.listen(3000);
